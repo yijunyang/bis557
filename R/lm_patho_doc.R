f87f101a96f08cd8eed13c929bfc8b8499c1e42c
yijunyang <47420382+yijunyang@users.noreply.github.com>
2020-09-14 23:27:39 -0400

    Initial commit

---
+++ R/lm_patho_doc.R
@@ -0,0 +1,8 @@
+#' @title DATA lm_patho
+#' @name lm_patho
+#' @docType data
+#' @rdname lm_patho
+#' @description This is the data for the 1st assignment of BIS557
+#' @keywords data
+
+NULL
---
+++ data-raw/lm_patho.R
@@ -0,0 +1,2 @@
+lm_patho <- read.csv("./data-raw/lm_patho.csv")
+use_data(lm_patho)
