1ddbaff2d02b8cd003d6797d0d189e83ed2f2d48
yijunyang <47420382+yijunyang@users.noreply.github.com>
2020-11-22 22:13:46 -0500

    upload hw4

--- R/ridge_regression.R
+++ R/ridge_regression.R
@@ -1,21 +1,3 @@
-# Define a function for making model matrices
-#' @title Make Model Matrices
-#' @author Yijun Yang
-#' @description Make a model matrix easily.
-#' @param form a formula
-#' @param d a dataframe
-#' @param contrasts contrasts argument
-#' @return X and Y for the model
-#' @export
-
-make_model_matrices <- function(form, d, contrasts = NULL){
-  d_no_na <- model.frame(form, d)
-  X <- model.matrix(form, d_no_na, contrasts.arg = contrasts)
-  y_name <- as.character(form)[2]
-  Y <- matrix(d_no_na[, y_name], ncol = 1)
-  list(X=X, Y=Y)
-}
-
 #' @title Ridge Regression
 #' @author Yijun Yang
 #' @description Implement a ridge regression function taking into account colinear (or nearly colinear) regression variables.
